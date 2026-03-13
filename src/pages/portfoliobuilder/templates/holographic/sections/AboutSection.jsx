e0c761582ed80060b3834e77b9af568ccff58ee8
Md Bakhtiyar <mdbakhtiyar098@gmail.com>
2026-03-14 00:30:38 +0530

    import issue

--- src/pages/portfoliobuilder/templates/holographic/sections/AboutSection.jsx
+++ src/pages/portfoliobuilder/templates/holographic/sections/AboutSection.jsx
@@ -1,4 +1,4 @@
-import { getSkillLogo } from "../../components/categorizeSkills";
+import { getSkillLogo } from "../../../components/categorizeSkills";
 
 function AboutSection({ data }) {
   return (
